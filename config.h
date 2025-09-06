50f0dfc43b478e7d4cde60516e817a4797005abe
AlfasWeb <108786369+AlfasWeb@users.noreply.github.com>
2025-09-05 23:13:16 -0300

    Update config.h

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <vector>
+#include <EEPROM.h>
 
 struct Config {
   String chave;
@@ -10,10 +11,16 @@ struct Config {
 };
 
 bool carregarArquivo(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
-bool salvarArquivo(const std::vector<Config>& lista, uint32_t addrIni, uint32_t addrFim);
+bool salvarArquivo(const std::vector<Config>& lista, uint32_t addrIni);
 String getValor(const std::vector<Config>& lista, const String& chave, const String& idioma = "");
 bool setValor(std::vector<Config>& lista, const String& chave, const String& valor, const String& idioma = "");
 void carregarArquivoParcial(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim, const String& idioma = "");
 void carregarImagem(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
+
+//EEPROM
+#define EEPROM_SIZE 512
+
+bool carregarDadosEEPROM(std::vector<Config>& lista);
+bool salvarDadosEEPROM(const std::vector<Config>& lista) ;
 
 #endif
